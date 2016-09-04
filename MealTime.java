120ed7ac3220f9f6e807b786f8d0d29b7bac57a2
Sara Jensen <sara.jensen@idealist.org>
2016-09-04 16:51:54 -0700

    move console myConsole = system.console(); below in the definition of the custom method so yourMeal could read input.

--- MealTime.java
+++ MealTime.java
@@ -2,7 +2,6 @@ import java.io.Console;
 
 public class MealTime {
   public static void main(String[] args) {
-    Console myConsole = System.console();
 
     // System.out.println("What did you eat for breakfast?");
     // replaces the above code with the question outlined in the custom method defined below.
@@ -21,8 +20,10 @@ public class MealTime {
 
   public static void askWhatYouAteFor(String meal) {
     // ADD HERE to DRY code
+    Console myConsole = System.console();
+
     System.out.println("What did you eat for " + meal + "?");
     String yourMeal = myConsole.readLine();
-    System.out.println("You had " + yourMeal " for " + meal + ".");
+    System.out.println("You had " + yourMeal + " for " + meal + ".");
   }
 }
